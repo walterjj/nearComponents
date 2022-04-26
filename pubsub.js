1efec6754b0e67bf0ddee57578318f4dcc11097e
walterjj <walterjj@gmail.com>
2022-04-25 23:44:47 -0300

    added generic mqtt in pubsub.js

--- pubsub.js
+++ pubsub.js
@@ -2,19 +2,24 @@
 
 import {LitElement, html, css} from "lit-element";
 import {NearUser} from "./user";
-//import * as MQTT from '/mqtt.min.js'
+//import * as MQTT from './mqtt.min.js';
+import * as MQTT from 'mqtt/dist/mqtt.min.js';
+
 
 let isLoaded=false;
 async function initMqtt() {
         if(!isLoaded)
-                await import('./mqtt.min.js')
+        {        await import('./mqtt.min.js');
+                 console.log("mqtt loaded", JSON.stringify(mqtt));
+        }
        isLoaded=true;         
 }
 
 export class NearMqtt extends LitElement {
         static get properties() {
                 return {
-                  connected: { type: Boolean, reflect: true }
+                  connected: { type: Boolean, reflect: true },
+                  endpoint: { type: String }
                 };
         }
 
@@ -45,6 +50,7 @@ export class NearMqtt extends LitElement {
         
         onMessage(topic,msg,packet){
                 console.log("recv:",topic,String(msg));
+                this.dispatchEvent(new CustomEvent('message', { detail: {topic:topic,msg:msg} }));
 
         }
 
@@ -62,35 +68,57 @@ export class NearMqtt extends LitElement {
                 this.client.publish(topic, msg);
         }
 
-        connect(){
-                initMqtt();
+        setupEvents(){
+                this.client.on('connect', function () {
+                        console.log('connect');
+                        this.connected=true;
+                        this.setAttribute("connected","");
+                        this.dispatchEvent(new CustomEvent('connect'));
+                             
+                }.bind(this))
+                this.client.on('disconnect', function () {
+                          console.log('disconnect');
+                          this.connected=false;
+                          this.dispatchEvent(new CustomEvent('disconnect'));        
+                }.bind(this))
+                this.client.on('close', function () {
+                          console.log('close');
+                          this.connected=false;
+                          this.dispatchEvent(new CustomEvent('close'));        
+                }.bind(this))
+                this.client.on('offline', function () {
+                          console.log('offline');
+                          this.connected=false;
+                          this.dispatchEvent(new CustomEvent('offline'));        
+                }.bind(this))
+                this.client.on('error', function (error) {
+                          console.log('error',error);     
+                }.bind(this))
+                      
+                this.client.on('message',this.onMessage.bind(this));
+
+        }
+
+        connect(options=null){
+                //initMqtt();
+                if(!options) options={
+                        reconnectPeriod: 10000,
+                        clientId: 'nearuser_' + Math.random().toString(16).substr(2, 8)
+                };
+                console.log("connecting to",this.endpoint,options);               
+                if(this.endpoint) {
+                        return new Promise((resolve,reject)=>{
+                                this.client=window.mqtt.connect(this.endpoint,options);
+                                this.setupEvents();
+                                resolve();
+                        });
+                };
                 return new Promise((resolve)=>{
                         NearUser.instance.getApi("ioturl").then((data)=>{
                                 console.log(data.url);  
-                                this.client=mqtt.connect(data.url,{clientId:"nearuser"});
+                                this.client=window.mqtt.connect(data.url,options);
                                 console.log(this);
-                                this.client.on('connect', function () {
-                                      console.log('connect');
-                                      this.connected=true;
-                                      this.setAttribute("connected","");        
-                                }.bind(this))
-                                this.client.on('disconnect', function () {
-                                        console.log('disconnect');
-                                        this.connected=false;        
-                                }.bind(this))
-                                this.client.on('close', function () {
-                                        console.log('close');
-                                        this.connected=false;        
-                                }.bind(this))
-                                this.client.on('offline', function () {
-                                        console.log('offline');
-                                        this.connected=false;        
-                                }.bind(this))
-                                this.client.on('error', function (error) {
-                                        console.log('error',error);     
-                                }.bind(this))
-                                    
-                                this.client.on('message',this.onMessage);
+                                this.setupEvents();
                                 resolve();
                           })
                 })                
